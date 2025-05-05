ade99e9f7fd188b4b5de4bf603b7810e060de909
default <Joyce.nkengbeza@stu.ocsb.ca>
2025-05-05 16:49:28 +0000

    date

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Joyce Nkengbeza
-// Created on: Mar 2025
+// Created on: April 2025
 // This file contains the JS functions for index.html
 
 'use strict'
